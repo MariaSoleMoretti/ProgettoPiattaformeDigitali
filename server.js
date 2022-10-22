b441769190ac11466c9ec0b4fc975a62ec6725cb
Glitch (glitch-hello-node) <none>
2022-10-22 13:37:12 +0000

    🚧🏎 Checkpoint ./server.js:44341290/380

--- server.js
+++ server.js
@@ -7,8 +7,13 @@ app.listen(3000);
 
 app.use(express.json());
 
+// using app.use to serve up static CSS files in public/assets/ folder when /public link is called in ejs files
+// app.use("/route", express.static("foldername"));
+app.use('/public', express.static('public'));
+
 //permette di separare il codice javescript da quello html
 app.set("view-engine", "ejs");
+
 
 //homepage del sito
 app.get("/", (req, res) => {
