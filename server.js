32e2b08aadc75759969e66a0c2f643718b31a719
Glitch (glitch-hello-node) <none>
2022-10-22 14:49:10 +0000

    🍶⏱ Checkpoint ./server.js:44341290/6 ./tutors.json:44341290/425

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.post("/addTutor", (req,res) =>{
       id:        Date.now().toString(),
       name:      req.body.name,
       email:     req.body.email,
-      universita:req.body.università,
+      università:req.body.università,
       corso:     req.body.corso
     });
     
