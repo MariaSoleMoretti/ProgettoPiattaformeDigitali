188824f7749187531dd866e6500200b4bbbf630a
Glitch (progetto-piattaforme-territorio) <none>
2022-11-18 08:11:30 +0000

    🍪🛩 Checkpoint ./server.js:44341290/16265

--- server.js
+++ server.js
@@ -6,12 +6,10 @@ const fs = require("fs");
 app.listen(3000);
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-
+app.use(express.urlencoded({ extended: true }));
 
 //permette di separare il codice javescript da quello html
 app.set("view-engine", "ejs");
-
 
 //dichiarazione della variabili globali
 let users = [];
@@ -22,57 +20,154 @@ app.get("/home", (req, res) => {
   res.render("home.ejs");
 });
 
-
 //pagina del login
 app.get("/home/addTutor", (req, res) => {
   res.render("addTutor.ejs");
 });
 
-app.post("/home/addTutor", (req,res) =>{
-  
+app.post("/home/addTutor", (req, res) => {
   //salvataggio del tutor del database
   let esito = validazioneInput(req.body.email, users);
-  console.log(esito);
-  if(esito == false){
-      res.send("Errore! L'email non è valida");
+  if (esito == false) {
+    console.log("Errore! L'email non è valida");
+    res.redirect("/badRequest");
+  } else {
+    //facciamo la read del file per modificarlo
+    let data = fs.readFileSync("tutors.json");
+    users = JSON.parse(data);
+
+    //aggiungo il tutor all'array dei tutor
+    users.push({
+      id: Date.now().toString(),
+      nome: req.body.nome,
+      cognome: req.body.cognome,
+      email: req.body.email,
+      universita: req.body.universita,
+      corso: req.body.corso,
+    });
+    //effettuo il writeback nel file
+    data = JSON.stringify(users, null, 2);
+    fs.writeFileSync("tutors.json", data);
+
+    console.log("File written successfully");
+    console.log(users);
+    res.redirect("/approved");
   }
-  else{
-    try {
-      //facciamo la read del file per modificarlo
-      let data = fs.readFileSync("tutors.json");
-      users = JSON.parse(data);
-   
-      //aggiungo il tutor all'array dei tutor
-      users.push({
-        id:        Date.now().toString(),
-        nome:      req.body.nome,
-        cognome:   req.body.cognome,
-        email:     req.body.email,
-        universita:req.body.universita,
-        corso:     req.body.corso
-      });
-      //effettuo il writeback nel file
-      data = JSON.stringify(users, null, 2);
-      
+});
+
+//api che filtra i tutor in base all'università
+app.get("/home/cercaUniversita", (req, res) => {
+  const uni = req.query.universita.toString();
+  let data = fs.readFileSync("tutors.json");
+
+  if (data != null) {
+    const tutors = JSON.parse(data);
+    //filtra i tutor in base all'università ricercata
+    let utentiRicercati = tutors.filter(
+      ricercaUniversità,
+      req.query.universita
+    );
+    res.setStatus(200).json(utentiRicercati);
+  } else {
+    res.redirect("/denied");
+  }
+});
+
+//api che filtra i tutor in base all'università e il corso
+app.get("/home/cercaUniversitaCorso", (req, res) => {
+  const uni = req.query.universita.toString();
+  let data = fs.readFileSync("tutors.json");
+
+  if (data != null) {
+    const tutors = JSON.parse(data);
+    //prima filtra tutti i tutor appartenenti all'università cercata
+    let utentiRicercatiUni = tutors.filter(
+      ricercaUniversità,
+      req.query.universita
+    );
+    //successivamente filtriamo l'array rusultante dall'operazione precedente
+    //per cercare i tutor di quella università che seguino il corso ricercato
+    let utentiRicercati = utentiRicercatiUni.filter(
+      ricercaCorso,
+      req.query.corso
+    );
+    res.send(utentiRicercati);
+  } else {
+    res.send("Error");
+  }
+});
+
+//api che filtra i tutor in base all'università, nome e cognome
+app.get("/home/cercaUniversitaNomeCognome", (req, res) => {
+  const uni = req.query.universita.toString();
+  let data = fs.readFileSync("tutors.json");
+
+  if (data != null) {
+    const tutors = JSON.parse(data);
+    //prima filtra tutti i tutor appartenenti all'università cercata
+    let tutorsByUni = tutors.filter(ricercaUniversità, req.query.universita);
+    //poi filtra l'array risultante dall'operazione precedente in base al nome
+    let tutorsByNome = tutorsByUni.filter(ricercaNome, req.query.nome);
+    //infine filtra la''array risultante in base al cognome
+    let tutorRicercati = tutorsByNome.filter(ricercaCognome, req.query.cognome);
+    res.setStatus(200).json(tutorRicercati);
+  } else {
+    res.send("Error");
+  }
+});
+
+//pagina per la modifica dal database
+app.get("/home/deleteTutorById", (req, res) => {
+  res.render("deleteTutor.ejs");
+});
+
+//api per eliminare un utente dal database ricercandolo in base all'id
+app.delete("/home/deleteTutorById", (req, res) => {
+  //facciamo la read del file per modificarlo
+  let data = fs.readFileSync("tutors.json");
+  if (data != null) {
+    //effettuo il parsing
+    const tutors = JSON.parse(data);
+
+    //ricerca nell'array l'elemento con l'id richiesto
+    let idTutor = tutors.findIndex((element) => element.id == req.body.id);
+
+    //se esiste lo elimino
+    if (idTutor != -1) {
+      let deletedTutor = tutors.splice(idTutor, 1);
+      //effettua il write back degli elementi aggiornati
+      data = JSON.stringify(tutors, null, 2);
       fs.writeFileSync("tutors.json", data);
       console.log("File written successfully");
-      console.log(users);
+      console.log(tutors);
       res.redirect("/approved");
-    }catch (e){
-      //se ci sono problemi viene reindirizzato su register
-      console.log("ERRORE");
+    } else {
+      console.log("Non esiste nessun utente con id " + req.body.id);
       res.redirect("/denied");
     }
   }
-})
+});
 
-//funzione di validazione dehli input, in particolare controlla se l'email è valida, 
+//api per settare lo status
+app.get("/", (req, res) => {
+  res.sendStatus(404);
+});
+
+app.get("/badRequest", (req, res) => {
+  res.sendStatus(400);
+});
+
+app.get("/denied", (req, res) => {
+  res.sendStatus(404);
+});
+
+//funzione di validazione dehli input, in particolare controlla se l'email è valida,
 //cioè se cointiene il carattere @, e se non è presente nel database
-function validazioneInput(email,users){
+function validazioneInput(email, users) {
   let esito = true;
   let tutorsByEmail = users.filter(ricercaEmail, email);
   console.log(tutorsByEmail);
-  if((tutorsByEmail.lenght == 0) || (email.toString().indexOf("@") == -1)){
+  if (tutorsByEmail.lenght == 0 || email.toString().indexOf("@") == -1) {
     //se l'email è già presente oppure se l'email inserita non contiene il carettere @ l'email non è valida
     esito = false;
   }
@@ -81,133 +176,37 @@ function validazioneInput(email,users){
 }
 
 //funzione che filtra i tutor in base alla email
-function ricercaEmail(elemento, tutors){
+function ricercaEmail(elemento, tutors) {
   //console.log(this);
-  if (elemento.email.toString().toLowerCase() === this.toLowerCase()){
+  if (elemento.email.toString().toLowerCase() === this.toLowerCase()) {
     return true;
   }
 }
 
-//api che filtra i tutor in base all'università 
-app.get("/home/cercaUniversita", (req,res) =>{
-  const uni = req.query.universita.toString();
-  let data = fs.readFileSync("tutors.json");
-  
-  if(data != null){
-    const tutors = JSON.parse(data);
-    //filtra i tutor in base all'università ricercata
-    let utentiRicercati = tutors.filter(ricercaUniversità, req.query.universita);
-    res.send(utentiRicercati);
-  }
-  else{
-    res.send('Error');
-  }
-})
-
-//api che filtra i tutor in base all'università e il corso
-app.get("/home/cercaUniversitaCorso", (req,res) =>{
-  const uni = req.query.universita.toString();
-  let data = fs.readFileSync("tutors.json");
-  
-  if(data != null){
-    const tutors = JSON.parse(data);
-    //prima filtra tutti i tutor appartenenti all'università cercata
-    let utentiRicercatiUni = tutors.filter(ricercaUniversità, req.query.universita);
-    //successivamente filtriamo l'array rusultante dall'operazione precedente
-    //per cercare i tutor di quella università che seguino il corso ricercato
-    let utentiRicercati = utentiRicercatiUni.filter(ricercaCorso, req.query.corso);
-    res.send(utentiRicercati);
-  }
-  else{
-    res.send('Error');
-  }
-})
-
-//api che filtra i tutor in base all'università, nome e cognome
-app.get("/home/cercaUniversitaNomeCognome", (req,res) =>{
-  const uni = req.query.universita.toString();
-  let data = fs.readFileSync("tutors.json");
-  
-  if(data != null){
-    const tutors = JSON.parse(data);
-    //prima filtra tutti i tutor appartenenti all'università cercata
-    let tutorsByUni = tutors.filter(ricercaUniversità, req.query.universita);
-    //poi filtra l'array risultante dall'operazione precedente in base al nome
-    let tutorsByNome = tutorsByUni.filter(ricercaNome, req.query.nome);
-    //infine filtra la''array risultante in base al cognome
-    let tutorRicercati = tutorsByNome.filter(ricercaCognome, req.query.cognome);
-    console.log(tutorRicercati);
-    res.send(tutorRicercati);
-  }
-  else{
-    res.send('Error');
-  }
-})
-
 //funzione per la ricerca dei tutor in base all'università
 function ricercaUniversità(elemento, tutors) {
-  if (elemento.universita.toString().toLowerCase() === this.toLowerCase()){
+  if (elemento.universita.toString().toLowerCase() === this.toLowerCase()) {
     return true;
   }
 }
 
 //funzione per la ricerca dei tutor in base al corso
 function ricercaCorso(elemento, tutors) {
-  if (elemento.corso.toString().toLowerCase() === this.toLowerCase()){
+  if (elemento.corso.toString().toLowerCase() === this.toLowerCase()) {
     return true;
   }
 }
 
 //funzione per la ricerca dei tutor in base al nome
 function ricercaNome(elemento, tutors) {
-  if (elemento.nome.toString().toLowerCase() === this.toLowerCase()){
+  if (elemento.nome.toString().toLowerCase() === this.toLowerCase()) {
     return true;
   }
 }
 
 //funzione per la ricerca dei tutor in base al cognome
 function ricercaCognome(elemento, tutors) {
-  if (elemento.cognome.toString().toLowerCase() === this.toLowerCase()){
+  if (elemento.cognome.toString().toLowerCase() === this.toLowerCase()) {
     return true;
   }
 }
-  
-//pagina per la modifica dal database
-app.get("/home/deleteTutorById",(req, res) => {
-  res.render("deleteTutor.ejs");
-});
-
-app.delete("/home/deleteTutorById", (req,res) =>{
-  
-  //facciamo la read del file per modificarlo
-  let data = fs.readFileSync("tutors.json");
-  if(data != null){
-    const tutors = JSON.parse(data);
-    
-    //ricerca nell'array l'elemento con l'id richiesto
-    let idTutor = tutors.findIndex((element) => element.id == req.body.id);
-    
-    //se esiste lo elimino
-    if(idTutor != -1){
-      let deletedTutor = tutors.splice(idTutor,1);
-      //effettua il write back degli elementi aggiornati
-      data = JSON.stringify(tutors, null, 2);
-      fs.writeFileSync("tutors.json", data);
-      console.log("File written successfully");
-      console.log(tutors);
-      res.redirect("/approved");
-    }
-    else{
-      console.log("Non esiste nessun utente con id " + req.body.id);
-      res.redirect("/denied");
-    }
-  }
-})
-
-app.get("/approved", (req,res)=>{
-  res.sendStatus(200);
-})
-
-app.get("/denied", (req,res)=>{
-  res.sendStatus(404);
-})
