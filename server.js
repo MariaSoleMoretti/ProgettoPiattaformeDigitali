6e561c460fec8aa05c63766007ef5765222f0695
Glitch (progetto-piattaforme-territorio) <none>
2022-11-20 20:45:00 +0000

    🎹⛱ Checkpoint ./server.js:44341290/19 ./views/addTutor.html:44341290/205 ./tutors.json:44341290/199

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.post("/home/addTutor", (req, res) => {
     
     //aggiungo il tutor all'array dei tutor
     tutors.push(req.body);
+    console.log(tutors);
     
     //effettuo il writeback nel file
     data = JSON.stringify(tutors, null, 2);
