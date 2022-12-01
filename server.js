9ba527aa62630df419e42a1a3570c82fd9a365c8
Glitch (progetto-piattaforme-territorio) <none>
2022-12-01 11:22:45 +0000

    📊🚀 Checkpoint ./server.js:44341290/319

--- server.js
+++ server.js
@@ -165,6 +165,7 @@ app.delete("/home/deleteTutor", (req, res) => {
 
 //api di aggiornamento lista esami dei singoli tutor
 app.put("/home/addExam", (req,res) =>{
+  let tutorRicercato = [];
   //facciamo la read del file per modificarlo
   let data = fs.readFileSync("tutors.json");
   tutors = JSON.parse(data);
@@ -176,9 +177,9 @@ app.put("/home/addExam", (req,res) =>{
   //se esiste lo elimino
   if (idTutor != -1) {
     //se il tutor è stato trovato effettuo la modifica
-    let tutorRicercato = tutors.filter(ricercaId, req.body.id);
+    tutorRicercato[0] = tutors.splice(idTutor, 1);
+    console.log("Il tutor ricercato è: ")
     console.log(tutorRicercato);
-    tutors.splice(idTutor, 1);
     tutorRicercato.esami.push(req.body.newExam);
 
     //effettuo il writeback
