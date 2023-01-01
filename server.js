64c0ef21a97025401684bd43ba5bedf2f55a8fc2
Glitch (progetto-piattaforme-territorio) <none>
2023-01-01 17:28:52 +0000

    💘🐫 Checkpoint ./views/dataTable.html:44341290/2 ./server.js:44341290/195

--- server.js
+++ server.js
@@ -254,14 +254,15 @@ function ricercaEmail(elemento) {
 
 //funzione per la ricerca dei tutor in base all'università
 function ricercaUniversità(elemento) {
-  if (elemento.universita.toString().toLowerCase() === this.toString().toLowerCase()) {
+  console.log(elemento.universita.toString().toLowerCase().contains(this.toString().toLowerCase()));
+  if (elemento.universita.toString().toLowerCase().contains(this.toString().toLowerCase())) {
     return true;
   }
 }
 
 //funzione per la ricerca dei tutor in base al corso
 function ricercaCorso(elemento) {
-  if (elemento.corso.toString().toLowerCase() === this.toString().toLowerCase()) {
+  if (elemento.corso.toString().toLowerCase().contains(this.toString().toLowerCase())) {
     return true;
   }
 }
